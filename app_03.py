ae2407d07f448667ecfd86fda70117cc3285b0db
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-04-03 19:41:55 +0900

    Update app_03.py

--- app_03.py
+++ app_03.py
@@ -5,22 +5,38 @@ from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv(verbose=True)
 key = os.getenv('OPENAI_API_KEY')
 
 # 1. 프롬프트
 prompt = PromptTemplate.from_template(
-    "{product}는 어는 회사에서 만든 제품인가요?"
+    """당신은 영어를 가르치는 10년차 영어 선생님입니다.
+상황에 맞는 영어 회화를 작성해 주세요.
+양식은 [FORMAT]을 참고하여 작성해 주세요.
+
+#상황:
+{question}
+
+#FORMAT:
+- 영어 회화:
+- 한글 해석:
+"""
 )
 
 # 2. LLM
-llm = ChatOpenAI(api_key=key,model='gpt-4o-mini')
+llm = ChatOpenAI(
+    api_key=key,
+    model='gpt-4o-mini'
+)
 
 # 3. 출력 파서
 output_parser = StrOutputParser()
 
-# 체인 만들기(실행기)
+# chain (실행기)
 chain = prompt | llm | output_parser
 
-answer = chain.invoke({'product': '겔럭시폰'})
+user_input = input('질문: ')
+answer = chain.invoke({'question': user_input})
+
 print(answer)
