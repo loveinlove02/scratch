6bc3f1feca8b09a05eb1d2415caf94864021d61d
LEE IN-HWAN <59072437+loveinlove02@users.noreply.github.com>
2025-05-29 19:42:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ load_dotenv()
 key = os.getenv('OPENAI_API_KEY')
 
 st.title('챗봇')
+
+
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = []
+
+def print_message():
+    for chat_message in st.session_state['messages']:
+        with st.chat_message(chat_message.role):
+            st.write(chat_message.content)
+
+def add_message(role, message):
+    st.session_state['messages'].append(ChatMessage(role=role, content=message))
+
+add_message(role='user', message='안녕?')
+add_message(role='assistant', message='무엇을 도와드릴까요?')
+
+print_message()
